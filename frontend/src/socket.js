7c668749397e036a22cb970cf5d0c228d8a94f94
l1nky358 <prokotrob@gmail.com>
2026-03-18 14:58:36 +0700

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5001', {
+const socket = io(process.env.NODE_ENV === 'production' 
+  ? window.location.origin 
+  : 'http://localhost:5001', {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 5,
