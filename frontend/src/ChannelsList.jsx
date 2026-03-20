86219309aa5f10071d4fc98c9a23568844b98883
l1nky358 <prokotrob@gmail.com>
2026-03-20 14:36:14 +0700

    Update ChannelsList.jsx

--- frontend/src/ChannelsList.jsx
+++ frontend/src/ChannelsList.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useTranslation } 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import ChannelMenu from './ChannelMenu';
 import AddChannelModal from './modals/AddChannelModal';
 import RenameChannelModal from './modals/RenameChannelModal';
