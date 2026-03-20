4af30f5f1ff2f4a680988fda05847765d214e1fb
l1nky358 <prokotrob@gmail.com>
2026-03-20 14:49:45 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider, useAuth } from './AuthContext';
 import Header from './Header';
 import Chat from './Chat';
 import Login from './Login';
@@ -8,27 +9,29 @@ import Signup from './Signup';
 import NotFound from './NotFound';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
+  const { token } = useAuth();
   return token ? children : <Navigate to="/login" replace />;
 };
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="d-flex flex-column vh-100">
-        <Header />
-        <div className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-          </Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="d-flex flex-column vh-100">
+          <Header />
+          <div className="flex-grow-1">
+            <Routes>
+              <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/404" element={<NotFound />} />
+              <Route path="*" element={<Navigate to="/404" replace />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-      <ToastContainer />
-    </BrowserRouter>
+        <ToastContainer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
