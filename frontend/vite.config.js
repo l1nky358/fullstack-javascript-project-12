57f38f44c865c22f3d317c8e98ecc2f666f021fc
Роберт <prokotrob@gmail.com>
2026-03-23 17:58:09 +0700

    Configure build to output to public folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../public',
+    emptyOutDir: true
+  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
         changeOrigin: true,
-        rewrite: (path) => path
       }
     }
   }
