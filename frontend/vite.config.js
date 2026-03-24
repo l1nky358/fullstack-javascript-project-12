36cefa37f2fa75cd0f075eb880a96e92ab3f407d
Роберт <prokotrob@gmail.com>
2026-03-24 20:52:30 +0700

    fhgfhghfghfgfgh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    outDir: '../public',
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
