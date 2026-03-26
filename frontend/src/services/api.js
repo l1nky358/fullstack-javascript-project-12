b5293ae2092081c49cfacb04e26527f2b905c53a
l1nky358 <prokotrob@gmail.com>
2026-03-26 14:47:09 +0700

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,73 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  tagTypes: ['Channels', 'Messages'],
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: '/login',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+    signup: builder.mutation({
+      query: (userData) => ({
+        url: '/signup',
+        method: 'POST',
+        body: userData,
+      }),
+    }),
+    getChannels: builder.query({
+      query: () => '/channels',
+      providesTags: ['Channels'],
+    }),
+    addChannel: builder.mutation({
+      query: (name) => ({
+        url: '/channels',
+        method: 'POST',
+        body: { name },
+      }),
+      invalidatesTags: ['Channels'],
+    }),
+    renameChannel: builder.mutation({
+      query: ({ id, name }) => ({
+        url: `/channels/${id}`,
+        method: 'PATCH',
+        body: { name },
+      }),
+      invalidatesTags: ['Channels'],
+    }),
+    removeChannel: builder.mutation({
+      query: (id) => ({
+        url: `/channels/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Channels'],
+    }),
+    getMessages: builder.query({
+      query: () => '/messages',
+      providesTags: ['Messages'],
+    }),
+    addMessage: builder.mutation({
+      query: (message) => ({
+        url: '/messages',
+        method: 'POST',
+        body: message,
+      }),
+      invalidatesTags: ['Messages'],
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useSignupMutation,
+  useGetChannelsQuery,
+  useAddChannelMutation,
+  useRenameChannelMutation,
+  useRemoveChannelMutation,
+  useGetMessagesQuery,
+  useAddMessageMutation,
+} = api;
