972de7e69170c476969315da625a198bee880ddc
l1nky358 <prokotrob@gmail.com>
2026-03-26 14:49:58 +0700

    Delete frontend/src/rollbar.js

--- frontend/src/rollbar.js
+++
@@ -1,8 +0,0 @@
-const rollbarConfig = {
-  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
-  environment: import.meta.env.MODE,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-};
-
-export default rollbarConfig;
