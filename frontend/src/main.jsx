18bd9e609943aa4e53499f27d883ecb704614e30
l1nky358 <prokotrob@gmail.com>
2026-03-26 14:46:30 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-import { AuthProvider } from './AuthContext';
-import store from './store.js';
-import i18n from './i18n.js';
-import App from './App.jsx';
+import store from './store';
+import i18n from './i18n';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -30,9 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       >
         <ReduxProvider store={store}>
           <I18nextProvider i18n={i18n}>
-            <AuthProvider>
-              <App />
-            </AuthProvider>
+            <App />
           </I18nextProvider>
         </ReduxProvider>
       </ErrorBoundary>
