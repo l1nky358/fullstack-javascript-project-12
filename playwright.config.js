3ae593001483e7c6a7fc266471839f519a65c642
l1nky358 <prokotrob@gmail.com>
2026-03-27 18:05:44 +0700

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   webServer: {
-    command: 'npm start',
+    command: 'node test-server.js',
     port: 5001,
     timeout: 60000,
     reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    stderr: 'pipe',
   },
   use: {
     baseURL: 'http://localhost:5001',
