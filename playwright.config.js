779ad295821b778dc245d6cc56bb9193df4c84c0
Роберт <prokotrob@gmail.com>
2026-03-29 13:58:30 +0700

    im tired

--- playwright.config.js
+++ playwright.config.js
@@ -1,36 +1,19 @@
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig } from '@playwright/test';
 
 const baseURL = process.env.BASE_URL || 'http://localhost:5001';
 
-module.exports = defineConfig({
-  testDir: './',
+export default defineConfig({
+  testDir: './tests',
+  retries: 1,
   timeout: 30000,
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  
   use: {
     baseURL: baseURL,
     trace: 'on-first-retry',
   },
-
   webServer: {
     command: 'npm run start',
     port: 5001,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-    stdout: 'pipe',
-    stderr: 'pipe',
-    // В Docker не нужно запускать webServer отдельно
-    // потому что сервер уже запущен в отдельном контейнере
+    timeout: 300000, 
+    reuseExistingServer: false,
   },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
 });
