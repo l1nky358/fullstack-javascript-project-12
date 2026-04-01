95a8dafda947ea1ddfb51d3b1b3bee1b26471ca8
l1nky358 <prokotrob@gmail.com>
2026-04-01 13:07:39 +0700

    Delete playwright.config.js

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  testDir: './',
-  timeout: 30000,
-  use: {
-    baseURL: 'http://localhost:5001',
-  },
-});
