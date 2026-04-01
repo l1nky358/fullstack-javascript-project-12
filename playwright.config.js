e474205a856cbd5bb36e1ab2abc553cff7fe1a72
l1nky358 <prokotrob@gmail.com>
2026-04-01 13:02:54 +0700

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './',
+  timeout: 30000,
+  use: {
+    baseURL: 'http://localhost:5001',
+  },
+});
