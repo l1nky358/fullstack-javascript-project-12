b326d9fbdd4d6247cbec80670c9c3a461cf73653
l1nky358 <prokotrob@gmail.com>
2026-04-02 18:32:03 +0700

    Update Login.jsx

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -54,12 +54,6 @@ const Login = () => {
         </div>
         
         {/* Только один блок ошибки */}
-        {authError && (
-          <div className="auth-error">
-            {authError}
-          </div>
-        )}
-        
         <form onSubmit={formik.handleSubmit} className="auth-form">
           {/* ... остальной код формы без изменений ... */}
         </form>
