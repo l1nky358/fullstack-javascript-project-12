f40ba7029d3bfd0cb0f428ea4d5f06c59601ea2f
l1nky358 <prokotrob@gmail.com>
2026-04-02 17:53:07 +0700

    Update Toast.jsx

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -1,45 +1,17 @@
 import { toast } from 'react-toastify';
 
 export const showSuccess = (message) => {
-  toast.success(message, {
-    position: "top-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('Toast suppressed (success):', message);
 };
 
 export const showError = (message) => {
-  toast.error(message, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('Toast suppressed (error):', message);
 };
 
 export const showWarning = (message) => {
-  toast.warning(message, {
-    position: "top-right",
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('Toast suppressed (warning):', message);
 };
 
 export const showInfo = (message) => {
-  toast.info(message, {
-    position: "top-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('Toast suppressed (info):', message);
 };
