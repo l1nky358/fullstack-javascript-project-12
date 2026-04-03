a4aaeb4b5a5ccf567a393edbc82e87e88009b713
l1nky358 <prokotrob@gmail.com>
2026-04-03 13:54:15 +0700

    Update Toast.jsx

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -1,45 +1,17 @@
 import { toast } from 'react-toastify';
 
 export const showSuccess = (message) => {
-  toast.success(message, {
-    position: "top-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('🔇 Success blocked:', message);
 };
 
 export const showError = (message) => {
-  toast.error(message, {
-    position: "top-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('🔇 Error blocked:', message);
 };
 
 export const showWarning = (message) => {
-  toast.warning(message, {
-    position: "top-right",
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('🔇 Warning blocked:', message);
 };
 
 export const showInfo = (message) => {
-  toast.info(message, {
-    position: "top-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-  });
+  console.log('🔇 Info blocked:', message);
 };
