1ae9e8dd8b7f6977656f0b1dc68bfe29b773d1ee
l1nky358 <prokotrob@gmail.com>
2026-04-04 13:42:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -54,10 +54,10 @@ app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
   
   const user = users.find(u => u.username === username && u.password === password);
-  /*if (!user) {
+  if (!user) {
     return res.status(401).json({ error: 'Invalid credentials' });
   }
-  */
+  
   res.json({ token: `fake-jwt-token-${user.id}` });
 });
 
