f81b9fa399156ae3e5cf272faf6b58d9df5ff695
l1nky358 <prokotrob@gmail.com>
2026-04-04 13:14:39 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,35 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './store';
 import i18n from './locales/i18n';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const rollbarConfig = {
-  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
-  environment: import.meta.env.MODE,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-};
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RollbarProvider config={rollbarConfig}>
-    <ErrorBoundary
-      fallbackUI={() => (
-        <div style={{ padding: '20px', color: 'red' }}>
-          <h2>Oops, something went wrong.</h2>
-          <p>We've been notified and are looking into it.</p>
-        </div>
-      )}
-    >
-      <ReduxProvider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </ReduxProvider>
-    </ErrorBoundary>
-  </RollbarProvider>
+  <ReduxProvider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </ReduxProvider>
 );
