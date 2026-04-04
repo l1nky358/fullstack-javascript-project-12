57e9b2e2a5ef34bc238a68b8a2669a2481265463
l1nky358 <prokotrob@gmail.com>
2026-04-04 19:22:56 +0700

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Server is running', async ({ request }) => {
-  const response = await request.get('http://localhost:5001/health');
-  expect(response.status()).toBe(200);
-});
