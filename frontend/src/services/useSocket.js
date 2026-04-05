ccd8f91f1b66f68314e02489ca1894b9244c5753
l1nky358 <prokotrob@gmail.com>
2026-04-05 17:13:12 +0700

    Update useSocket.js

--- frontend/src/services/useSocket.js
+++ frontend/src/services/useSocket.js
@@ -1,44 +1,40 @@
-import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
+import { initSocket, getSocket, closeSocket } from '../socket';
 
 export const useSocket = () => {
-  const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (!token) return;
 
-    const newSocket = io('/', {
-      auth: { token },
-      autoConnect: true,
-      reconnection: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 1000,
-      transports: ['websocket', 'polling']
-    });
-
-    newSocket.on('connect', () => {
+    const socket = getSocket();
+    
+    const onConnect = () => {
       console.log('Socket connected');
       setIsConnected(true);
-    });
-
-    newSocket.on('disconnect', () => {
+    };
+    
+    const onDisconnect = () => {
       console.log('Socket disconnected');
       setIsConnected(false);
-    });
-
-    newSocket.on('connect_error', (error) => {
-      console.error('Socket connection error:', error);
-      setIsConnected(false);
-    });
-
-    setSocket(newSocket);
-
+    };
+    
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    
+    if (!socket.connected) {
+      initSocket(token);
+    } else {
+      setIsConnected(true);
+    }
+    
     return () => {
-      newSocket.disconnect();
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      closeSocket();
     };
   }, []);
 
-  return { socket, isConnected };
+  return { socket: getSocket(), isConnected };
 };
