5faf96bf31fe2062d2c036811ceddb9abe1ff9b8
l1nky358 <prokotrob@gmail.com>
2026-04-05 13:54:42 +0700

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import { api } from '../services/api';
 import channelsReducer from './channelsSlice';
 import authReducer from '../slices/authSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     channels: channelsReducer,
@@ -12,3 +12,5 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+
+export default store;
