898a3e28ad94ab0c3b73d847e5d9e055ed7d72b4
l1nky358 <prokotrob@gmail.com>
2026-04-05 19:02:10 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,15 @@ import store from './store';
 import i18n from './locales/i18n';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReduxProvider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </ReduxProvider>
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </ReduxProvider>
+  </React.StrictMode>
 );
