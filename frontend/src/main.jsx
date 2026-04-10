54f7c7907ef5147ac8a0a7127411ce5fb9c5d7e4
l1nky358 <prokotrob@gmail.com>
2026-04-10 14:34:16 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider as ReduxProvider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import store from './store';
-import i18n from './locales/i18n';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-import './index.css';
-import { initSocket } from './socket';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider as ReduxProvider } from 'react-redux'
+import { I18nextProvider } from 'react-i18next'
+import store from './store'
+import i18n from './locales/i18n'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
+import { initSocket } from './socket'
 
 // Инициализация сокета без токена пока
-initSocket(null);
+initSocket(null)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </I18nextProvider>
     </ReduxProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
