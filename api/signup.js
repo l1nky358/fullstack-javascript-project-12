c6d34db0c89084fb138144350de040d39cb19ea8
l1nky358 <prokotrob@gmail.com>
2026-04-10 13:11:37 +0700

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -12,6 +12,6 @@ export default async function handler(req, res) {
       token: 'test-token-' + Date.now(),
       username: username,
     })
-  } 
+  }
   return res.status(405).json({ error: 'Method not allowed' })
 }
